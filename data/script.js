e6405d036fcd025829dec976d58537bc0d4818f9
flapaa8 <yoquieroayudaroficial@gmail.com>
2020-08-13 12:21:07 -0300

    v1

---
+++ data/script.js
@@ -0,0 +1,27 @@
+//menu
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu() {
+    document.getElementById("move-content").classList.toggle('move-container-all')
+    document.getElementById("show-menu").classList.toggle('show-lateral')
+}
+//boton up
+document.getElementById("button-up").addEventListener("click", scrollUp);
+function scrollUp() {
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0) {
+        window.requestAnimationFrame(scrollUp)
+        window.scrollTo(0, currentScroll - (currentScroll / 10))
+    }
+}
+
+buttonUp = document.getElementById("button-up");
+window.onscroll = function(){
+    var scroll = document.documentElement.scrollTop;
+    if (scroll>500){
+       buttonUp.style.transform = "scale(1)"
+    }
+    else if(scroll<500){
+        buttonUp.style.transform = "scale(0)"
+    }
+}
